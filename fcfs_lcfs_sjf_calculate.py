aeb2bfeccc0d0202c633e331edf5c9a9ee1a5bb8
Damian Duchnowski <damianduchnowski1@gmail.com>
2020-01-06 07:13:50 +0100

    SJF done Created common calculation method for FCFS, LCFS & SJF

---
+++ fcfs_lcfs_sjf_calculate.py
@@ -0,0 +1,27 @@
+from statistics import mean
+
+
+def calculate_avereage_waiting_time(data):
+    # Tablica, w ktorej zapisuje sredni czas oczekiwania procesow dla kazdego ciagu (seq)
+    avg_waiting_times = []
+
+    for seq in data:
+        waiting_times_in_current_seq = []
+        for idx, process in enumerate(seq):
+            waiting_times_in_current_seq.append(sum(seq[:idx]))
+        avg_waiting_times.append(mean(waiting_times_in_current_seq))
+
+    return mean(avg_waiting_times)
+
+
+def calculate_average_turnaround_time(data):
+    # Tablica, w ktorej zapisuje sredni czas cyklu przetwarzania dla kazdego ciagu (seq)
+    avg_turnaround_times = []
+
+    for seq in data:
+        turnaround_times_in_current_seq = []
+        for idx, process in enumerate(seq):
+            turnaround_times_in_current_seq.append(sum(seq[:idx])+process)
+        avg_turnaround_times.append(mean(turnaround_times_in_current_seq))
+
+    return mean(avg_turnaround_times)
--- sjf.py
+++ sjf.py
@@ -4,10 +4,10 @@ from fcfs_lcfs_sjf_calculate import *
 
 def output():
     with open('output.txt', 'a') as f:
-        f.write('LCFS:' + '\n')
+        f.write('SJF:' + '\n')
         f.write('---------------------------------------' + '\n')
         f.write('Sredni czas oczekiwania: ' +
-                str(round(calculate_avereage_waiting_time(list(reversed(seq)) for seq in parse()), 2)) + '\n')
+                str(round(calculate_avereage_waiting_time(list(sorted(seq)) for seq in parse()), 2)) + '\n')
         f.write('Sredni czas cyklu przetwarzania: ' +
-                str(round(calculate_average_turnaround_time(list(reversed(seq)) for seq in parse()), 2)) + '\n')
+                str(round(calculate_average_turnaround_time(list(sorted(seq)) for seq in parse()), 2)) + '\n')
         f.write('\n')
